5d0714a2f0f09914578bf05420b94311e153694b
github-action-benchmark <github@users.noreply.github.com>
2025-07-04 09:20:41 +0000

    add Benchmark (hermit-bench) benchmark result for 8f6424d09d9e0d3d26d19e6915f9651f04a1743d

---
+++ data.js
@@ -0,0 +1,54 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1751620841814,
+  "repoUrl": "https://github.com/CarlWachter/kernel",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "CarlWachter",
+            "username": "CarlWachter",
+            "email": "carlwachter@web.de"
+          },
+          "committer": {
+            "name": "CarlWachter",
+            "username": "CarlWachter",
+            "email": "carlwachter@web.de"
+          },
+          "id": "8f6424d09d9e0d3d26d19e6915f9651f04a1743d",
+          "message": "proper syntax",
+          "timestamp": "2025-07-04T09:17:13Z",
+          "url": "https://github.com/CarlWachter/kernel/commit/8f6424d09d9e0d3d26d19e6915f9651f04a1743d"
+        },
+        "date": 1751620841602,
+        "tool": "hermit-bench",
+        "benches": [
+          {
+            "name": "startup_benchmark Build Time",
+            "value": 72.137297723,
+            "unit": "s",
+            "range": 0,
+            "group": "Build",
+            "plot_group": "none"
+          },
+          {
+            "name": "startup_benchmark File Size",
+            "value": 0.85589599609375,
+            "unit": "MB",
+            "range": 0,
+            "group": "File Size",
+            "plot_group": "none"
+          },
+          {
+            "name": "1 core",
+            "value": 0.29499562,
+            "unit": "s",
+            "range": 0,
+            "group": "General",
+            "plot_group": "Startup Time"
+          }
+        ]
+      }
+    ]
+  }
+}
